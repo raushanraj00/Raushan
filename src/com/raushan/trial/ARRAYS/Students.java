2c3292f62ecd58ec271dac2cc3fce809ed10a4be
Raushan Raj <raushanraj00650@gmail.com>
2026-07-20 12:27:25 +0530

    day 2

---
+++ src/com/raushan/trial/ARRAYS/Students.java
@@ -0,0 +1,35 @@
+package com.raushan.trial.ARRAYS;
+
+import java.util.Scanner;
+
+public class Students{
+    public int roll;
+    public String name;
+    Students(int roll, String name){
+        this.roll= roll;
+        this.name = name;
+    }
+    public static class storing_array_names {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter No. of Students:-");
+            int size = sc.nextInt();
+            Students[] arr = new Students[size];
+
+            for (int i = 0; i < size; i++) {
+                System.out.println("Enter details for Student " + (i + 1));
+                System.out.println("Enter roll Number :- ");
+                int roll = sc.nextInt();
+                sc.nextLine();
+                System.out.println("Enter Name:- ");
+                String Name = sc.nextLine();
+                arr[i] = new Students(roll, Name);
+            }
+
+            System.out.println("Student Details :- ");
+            for (int i = 0; i < size; i++) {
+                System.out.println("Student " + i + " : ("+ arr[i].roll + " " + arr[i].name + ") ");
+            }
+        }
+    }
+}
---
+++ src/com/raushan/trial/ARRAYS/primitive_and_non_array.java
@@ -0,0 +1,22 @@
+package com.raushan.trial.ARRAYS;
+
+public class primitive_and_non_array {
+    public static void main(String[] args) {
+        int[] arr = {10,20,40,50,60,80};
+        int n= arr.length;
+
+        System.out.println("Primitive Array :- ");
+        for (int i = 0; i < n; i++) {
+            System.out.printf(arr[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Non Primitive Array:- ");
+        String[] Names = {"Raushan", "Hello", "Morning"};
+        n = Names.length;
+        for (int i = 0; i < n; i++) {
+            System.out.println(Names[i]+", ");
+        }
+
+        System.out.println("1st Name is "+Names[0]);
+    }
+}

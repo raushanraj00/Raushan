756ae59ea33c1098d0cb9bd719df05b84b66a9b3
Raushan Raj <raushanraj00650@gmail.com>
2026-09-06 13:03:48 +0530

    oops yt

--- src/EXCEPTION_HANDLING/bank_withdrawl.java
+++ src/EXCEPTION_HANDLING/bank_withdrawl.java
@@ -2,6 +2,24 @@ package EXCEPTION_HANDLING;
 
 public class bank_withdrawl {
     static void withdraw(double balance, double amount){
+        if(amount<0){
+            throw new IllegalArgumentException("Balance Should be Positive");
+        }
+        if(amount > balance){
+            throw new IllegalArgumentException("Insufficient Balance.");
+        }
+        System.out.println("Withdrawl Successful : ₹"+amount );
+        System.out.println("Available Balance : ₹"+(balance-amount));
+    }
 
+    public static void main(String[] args) {
+        try{
+            withdraw(10000, 15000);
+        } catch (IllegalArgumentException e){
+            System.out.println("Error : "+e.getMessage());
+        }
+        finally {
+            System.out.println("Thankyou For using our bank. :) ");
+        }
     }
 }

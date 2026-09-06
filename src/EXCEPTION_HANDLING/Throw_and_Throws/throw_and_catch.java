65bf31062a555ac5a183271896dbf77085dc8df1
Raushan Raj <raushanraj00650@gmail.com>
2026-09-06 12:07:53 +0530

    oops yt

--- src/EXCEPTION_HANDLING/Throw_and_Throws/throw_and_catch.java
+++ src/EXCEPTION_HANDLING/Throw_and_Throws/throw_and_catch.java
@@ -1,4 +1,19 @@
 package EXCEPTION_HANDLING.Throw_and_Throws;
 
 public class throw_and_catch {
-}
+    public static void main(String[] args) {
+        int age = 9;
+        if (age < 10) {
+            try {
+                throw new ArithmeticException("You are not eligible!.");
+            } catch (ArithmeticException e) {
+                System.out.println("Exception Caught.");
+                System.out.println(e.getMessage());
+            }
+        }
+            else {
+                System.out.println("Eligible.");
+            }
+        }
+
+    }

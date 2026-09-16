d41b9d812cb8a862db76838d57fea053039b0cf0
Raushan Raj <raushanraj00650@gmail.com>
2026-09-17 00:10:33 +0530

    oops yt

--- src/CU_LMS/IO_OPERATIONS/file_creation.java
+++ src/CU_LMS/IO_OPERATIONS/file_creation.java
@@ -1,4 +1,21 @@
 package CU_LMS.IO_OPERATIONS;
-
+import java.io.File;
+import java.io.IOException;
 public class file_creation {
+    static File newfile = new File("newfile.create");
+
+    public static void main(String[] args) {
+        try{
+            if(newfile.createNewFile()){
+                System.out.println("File Created.");
+            }
+            else {
+                System.out.println("Error creating file.");
+            }
+        }
+        catch(IOException e){
+            System.out.println("Error ");
+        }
+    }
+
 }
---
+++ src/TREES/basic_tree_representation.java
@@ -0,0 +1,64 @@
+package TREES;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class basic_tree_representation {
+    static class node{
+        int data ;
+        node left;
+        node right;
+        public node(int key){
+            data = key;
+        }
+    }
+    public static void preorder(node root){
+        if(root == null){
+            return ;
+        }
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+    public static void inorder(node root){
+        if(root == null){
+            return ;
+        }
+        inorder(root.left);
+        System.out.print(root.data+ " ");
+        inorder(root.right);
+    }
+    public static void postorder(node root) {
+        if (root == null) {
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data + " ");
+    }
+
+
+    public static void main(String[] args) {
+
+        node root = new node(1);
+        root.left = new node(2);
+        root.right = new node(3);
+        root.left.right = new node(5);
+        root.left.left = new node(4);
+        root.right.left = new node(6);
+        root.right.right = new node(7);
+        preorder(root);
+
+//        System.out.println("Root data : "+ root.data);
+//        System.out.println("Left Child Data : "+root.left.data );
+//        System.out.println("Left Child Left data :"+ root.left.left.data);
+//        System.out.println("Left  Child Right data :"+ root.left.right.data);
+//
+//        System.out.println("Right Child data : "+ root.right.data);
+//
+//        System.out.println("Right Child Left data :"+ root.right.left.data);
+//        System.out.println("Right Child Right data :"+ root.right.right.data);
+
+
+
+    }
+}
